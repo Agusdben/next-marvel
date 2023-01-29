265acbf952a40be128e848d6a41c3fe727f362c6
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-29 14:42:18 -0300

    first commit

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import AppLayout from '@/components/AppLayout'
+import React from 'react'
+
+const About = () => {
+  return (
+    <AppLayout>
+      <h1>ABOUT</h1>
+    </AppLayout>
+  )
+}
+
+export default About
