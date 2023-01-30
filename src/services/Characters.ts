3d5c600def8f12a76bec78e8adaf4de7a16157f4
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-01-30 10:09:18 -0300

    little changes

--- src/services/Characters.ts
+++ src/services/Characters.ts
@@ -3,7 +3,6 @@ import { ApiCharacter } from '@/types/character'
 const { API_URL, API_HASH, API_TS, API_KEY } = process.env
 
 console.log(API_URL)
-//     'https://gateway.marvel.com/v1/public/characters?orderBy=modified&ts=1&apikey=fe865687581876389502f4712e08e4ac&hash=f3b239f383feda4a2a3b65c8059d6c52'
 
 const CHARACTERS_URL = `${API_URL}/characters`
 const CONFIG = `ts=${API_TS}&apikey=${API_KEY}&hash=${API_HASH}`
